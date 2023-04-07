213695d53f9d0b4adba76000f4ca3f4d1694810b
shaikat17 <shaikatpal56@gmail.com>
2023-04-07 16:17:47 +0600

    made some improvements on ui layout

--- App.js
+++ App.js
@@ -9,7 +9,7 @@ export default function App() {
         <TextInput style={styles.inputField} placeholder='Enter Your Course Goal'/>
         <Button title='Add Goal'/>
       </View>
-      <View>
+      <View style={styles.goalsContainer}>
         <Text>List Of Goals...</Text>
       </View>
     </View>
@@ -18,17 +18,27 @@ export default function App() {
 
 const styles = StyleSheet.create({
   appContainer: {
+    flex: 1,
     padding: 50,
+    paddingHorizontal: 16
   },
   inputContainer: {
+    flex: 1,
     flexDirection: 'row',
-    justifyContent: 'space-between'
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: '#cccccc'
   },
   inputField: {
     borderWidth: 1,
     borderColor: '#cccccc',
-    width: '80%',
+    width: '70%',
     marginRight: 8,
     padding: 8
+  },
+  goalsContainer: {
+    flex: 5
   }
 });
