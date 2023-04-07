6038b332802965e374bc68b50ae293f6ebd82eee
shaikat17 <shaikatpal56@gmail.com>
2023-04-07 20:27:42 +0600

    geting input field data successfuly

--- App.js
+++ App.js
@@ -1,13 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
 import { Button } from 'react-native';
 import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default function App() {
+  const [inputValue, setInputValue] = useState('')
+
+  const goalInputHandler = (enteredText) => {
+    setInputValue(enteredText)
+  }
+
+  const addGoalHandler = () => {
+    console.log(inputValue)
+    setInputValue('')
+  }
+
+
   return (
     <View style={styles.appContainer}>
       <View style={styles.inputContainer}>
-        <TextInput style={styles.inputField} placeholder='Enter Your Course Goal'/>
-        <Button title='Add Goal'/>
+        <TextInput style={styles.inputField} placeholder='Enter Your Course Goal' onChangeText={goalInputHandler}/>
+        <Button title='Add Goal' onPress={addGoalHandler}/>
       </View>
       <View style={styles.goalsContainer}>
         <Text>List Of Goals...</Text>
